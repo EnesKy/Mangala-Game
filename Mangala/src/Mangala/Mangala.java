6f195309cfa73a39b84961523dd54327eefa9036
Enes Kamil YILMAZ <eneskamil.yilmaz@stu.fsm.edu.tr>
2018-03-05 20:04:28 +0300

    Files added.

---
+++ Mangala/src/Mangala/Mangala.java
@@ -0,0 +1,22 @@
+package Mangala;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nskmlylm
+ */
+public class Mangala {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
