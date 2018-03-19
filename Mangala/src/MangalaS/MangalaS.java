127e2422b77b5bc7ee8f03eabdd768914efb239e
Enes Kamil YILMAZ <eneskamil.yilmaz@stu.fsm.edu.tr>
2018-03-19 15:11:37 +0300

    Client-Server Projects added.

---
+++ Mangala/src/MangalaS/MangalaS.java
@@ -0,0 +1,12 @@
+package MangalaS;
+
+/**
+ *
+ * @author nskmlylm
+ */
+public class MangalaS {
+    public static void main(String[] args) {
+        // TODO code application logic here    
+        //Server.Start(2000);
+    }
+}
